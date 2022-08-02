d285887826fa7f891a2d32569e1b976ba6d3f7a1
Valentin Barit <valentin.barit@gmail.com>
2022-08-02 14:21:23 +0200

    test: 💍 App renders inputs

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe(App.name, () => {
+  it("renders a name input", () => {
+    const { getByText } = render(<App />);
+    const inputNameElement = getByText(/John Doe/i);
+    expect(inputNameElement).toBeInTheDocument();
+  });
+
+  it("renders a title input", () => {
+    const { getByText } = render(<App />);
+    const inputTitleElement = getByText(/Super hero/i);
+    expect(inputTitleElement).toBeInTheDocument();
+  });
+
+  it("renders a phone number input", () => {
+    const { getByText } = render(<App />);
+    const inputPhoneNumberElement = getByText(/\+3316180339/i);
+    expect(inputPhoneNumberElement).toBeInTheDocument();
+  });
 });
